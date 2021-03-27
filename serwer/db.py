5998d12d776262eaae54d60bad87e26a6a3e0390
szche <github@chadam.pl>
2021-03-27 20:10:44 +0100

    Add features

--- serwer/db.py
+++ serwer/db.py
@@ -9,9 +9,10 @@ class Location(object):
         self.address = address
         self.size = size
         self.queue = []
+        self.wait_time = 0
 
     def __str__(self):
-        return f'-----\nLokacja: {self.id}\nNazwa: {self.name}\nAdres:{self.address}\nRozmiar:{self.size}\nKolejka:{self.queue}\n---'
+        return f'-----\nLokacja: {self.id}\nNazwa: {self.name}\nAdres: {self.address}\nRozmiar: {self.size}\nKolejka: {self.queue}\nW kolejce: {self.get_queue_size()}\n-----'
 
     def get_max_customers(self):
         if self.size <= 100:
@@ -19,17 +20,17 @@ class Location(object):
         else:
             return self.size//20
 
+    def get_queue_size(self):
+        return len(self.queue)
+
     def add_to_queue(self, customer):
-        for c in self.queue:
-            if customer.customerID == c.customerID: 
-                print("already in queue")
-                return False
+        if customer.customerID in self.queue:
+            print("already in queue")
+            return
         if len(self.queue) < self.get_max_customers():
-            self.queue.append(customer)
-            return True
+            self.queue.append(customer.customerID)
         else:
             print("queue is full")
-            return False
 
     def remove_from_queue(self, customer):
         if customer.customerID not in self.queue:
@@ -42,32 +43,30 @@ class DB(object):
         self.locations = {}
 
     def add_location(self, locationID, name, address, size):
-        #Sprawdz czy lokalizacja nie zostala juz dodana
+        print(self.locations)
         if locationID in self.locations.keys():
             print("Location already exists")
             return False
         self.locations[locationID] = Location(locationID, name, address, size)
+        print(self.locations)
         return True
 
     def get_location(self, locationID):
         if locationID in self.locations.keys():
             return self.locations[locationID]
 
-    def get_all(self):
-        return_data = []
+    def queue_index(self, customerID):
         for location in self.locations.values():
-            dc = {
-                'name': location.name,
-                'id': location.id,
-                'address': location.address,
-                'queue_size': len(location.queue),
-                'max_size': location.get_max_customers()
-            }
-            return_data.append(dc)
-        return return_data
+            if customerID in location.queue:
+                return (location.id, location.queue.index(customerID)+1)
+        return 0
+
+
+
 
 if __name__ == "__main__":
     db = DB()
+    db.add_location(123123, 'abc', 'ul', 120)
     db.add_location(123123, 'abc', 'ul', 120)
     db.add_location(1, 'xyz', 'al', 1000)
     
@@ -78,10 +77,16 @@ if __name__ == "__main__":
     customer_2 = Customer(2)
     customer_3 = Customer(3)
 
-    k = db.get_location(123123)
-    k.add_to_queue(customer_1)
-    # db.get_location(123123).add_to_queue(customer_3)
-    # db.get_location(123123).add_to_queue(customer_2)
+    db.get_location(123123).add_to_queue(customer_1)
+    db.get_location(123123).add_to_queue(customer_3)
+    db.get_location(123123).add_to_queue(customer_2)
 
     print(db.get_location(1))
     print(db.get_location(123123))
+
+    print(db.queue_index(customer_1.customerID))
+    print(db.queue_index(customer_2.customerID))
+    print(db.queue_index(customer_3.customerID))
+
+
+    
