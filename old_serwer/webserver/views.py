c5cf89f700cab45e19cf2685df96d9b13e126711
m-orlewski <michu.orlewski@gmail.com>
2021-03-27 19:19:22 +0100

    delete old files

--- old_serwer/webserver/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'webserver/index.html')
-
-def create(request):
-    return render(request, 'webserver/create.html')
