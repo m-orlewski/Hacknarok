b1f2ae6e3847c0045c709e320b1960e0e7ffc616
jaenek <jaenek@protonmail.com>
2021-03-27 18:55:06 +0000

    Encrypt data in QR code

--- czytnik/gen_qr.py
+++ czytnik/gen_qr.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import qrcode
 import qrcode.image.svg
+import aes
 
+# example use: make_qr('test.svg', 'client_id=1&direction=out')
 def make_qr(path, data):
-    img = qrcode.make(data, image_factory=qrcode.image.svg.SvgImage)
+    encrypted_data = aes.encrypt(data)
+    img = qrcode.make(encrypted_data, image_factory=qrcode.image.svg.SvgImage)
     img.save(path)
     
-
-make_qr('test.svg', '<adres>/start?customer_id=1&reader_id=1')
